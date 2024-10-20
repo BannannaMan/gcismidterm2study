7ba48b2d0fbb52b5258e669e4fd74418a0705417
BannannaMan <137525198+BannannaMan@users.noreply.github.com>
2024-10-20 16:35:59 -0400

    Add files via upload

---
+++ examPrep/study.py
@@ -0,0 +1,79 @@
+def fizzbuzz(max_value): 
+#    for value in range(max_value):
+    value = 0
+    while value <= max_value:
+        if value % 5 == 0 and value % 7 == 0:
+            print('fizzbuzz')
+        elif value % 5 == 0:
+            print('fizz')
+        elif value % 7 == 0:
+            print('buzz')
+        else:
+            print(value)
+        value += 1
+
+def is_palindrome(word):
+    backwards = ''
+    length = len(word) * -1
+    counter = -1
+    while counter >= length:
+        backwards += word[counter]
+        counter -= 1
+    if backwards == word:
+        return True
+    else:
+        print(backwards)
+        return False
+
+def harmonic_sum(n):
+    value = 1
+    sumed = 0
+    while value <= n:
+        current_number = 1/value
+        value += 1
+        sumed += current_number
+    return sumed
+
+def the_raven():
+    text = "Once upon a midnight dreary, while I pondered, weak and weary,\nOver many a quaint and curious volume of forgotten lore—\n\tWhile I nodded, nearly napping, suddenly there came a tapping,\nAs of some one gently rapping, rapping at my chamber door.\n\"\'Tis some visitor,\" I muttered,\"tapping at my chamber door—\n\t\tOnly this and nothing more.\""
+    print(text)
+
+def open_file(filename):
+    print("Time to open the file")
+    file = open(filename)
+    file.close()
+    print("File has been closed")
+
+def odd_lines(filename):
+    with open(filename) as file:
+        output = ''
+        count = 0
+        for line in file:
+            count +=1
+            if count % 2 == 1: 
+                output += line
+
+    return output
+
+def odd_lines2(filename):
+    with open(filename) as file:
+        count = 0
+        for line in file:
+            count += 1
+    return count
+
+
+
+def main():
+#    fizzbuzz(35)
+#    print(is_palindrome('penis'))
+#    print(harmonic_sum(100))
+#    the_raven()
+#    open_file('full_grades_100.csv')
+    print(odd_lines('grades_010.csv'))
+    print(odd_lines2('alice.txt'))
+
+
+
+if __name__ == "__main__":
+    main()
